05c08df61e265cd06b8698a6726e02ab8034130f
Milad Hobbi Mobarhan <miladh@fys.uio.no>
2013-04-23 14:28:50 +0200

    added ConfigParser

--- src/Minimizer/steepestdescent.cpp
+++ src/Minimizer/steepestdescent.cpp
@@ -41,10 +41,10 @@ void SteepestDescent::runMinimizer(){
 
         if(old(1)/variationalDerivate(1) >= 0){
             step*=1.25;
-            beta -= step*signFunc(variationalDerivate(0));
+            beta -= step*signFunc(variationalDerivate(1));
         }else{
             step*=0.5;
-            beta -= step*signFunc(variationalDerivate(0));
+            beta -= step*signFunc(variationalDerivate(1));
         }
 
         old = variationalDerivate;
